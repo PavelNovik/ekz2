f53df47a70d726e90673975e1b03bb897c9a0115
Pavel Novik <brn2befree@gmail.com>
2024-01-09 20:40:17 +0300

    run 16 ekz

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,63 @@
-import React from 'react';
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Types
+type PostType = {
+    id: string
+    body: string
+    title: string
+    userId: string
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// Api
+const instance = axios.create({baseURL: 'https://exams-frontend.kimitsu.it-incubator.ru/api/'})
+
+const postsAPI = {
+    getPosts() {
+        // Promise.resolve() стоит в качестве заглушки, чтобы TS не ругался и код компилировался
+        // Promise.resolve() нужно удалить и написать правильный запрос для получения постов
+        // return Promise.resolve()
+        return instance.get<PostType[]>('posts')
+    },
+}
+
+
+// App
+export const App = () => {
+
+    const [posts, setPosts] = useState<PostType[]>([])
+
+    useEffect(() => {
+        postsAPI.getPosts()
+            .then((res: any) => {
+                setPosts(res.data)
+            })
+    }, [])
+
+
+    return (
+        <>
+            <h1>📜 Список постов</h1>
+            {
+                posts.length
+                    ? posts.map(p => {
+                        return <div key={p.id}><b>title</b>: {p.title}</div>
+                    })
+                    : <h2>Постов нету 😥</h2>
+            }
+        </>
+    )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(<App/>)
+
+// 📜 Описание:
+// Напишите запрос на сервер для получения всех постов
+// Типизацию возвращаемых данных в ответе указывать необязательно, но можно и указать (в ответах учтены оба варианта).
+// Исправленную версию строки напишите в качестве ответа.
+
+// 🖥 Пример ответа: return Promise.resolve()
