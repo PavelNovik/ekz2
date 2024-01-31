3d5f973dacde4108282d37aa2245d7a5fb9dc9ac
Pavel Novik <brn2befree@gmail.com>
2024-01-31 13:45:07 +0300

    finish - 19 ekz

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,113 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes, useNavigate, useParams } from 'react-router-dom'
+import React from 'react'
 
-export const App = () => {
+type UserType = {
+    id: number
+    name: string
+    avatar: string
+    age: number
+    address: string
+}
+
+const users: UserType[] = [
+    {
+        id: 1,
+        name: 'my Name',
+        age: 32,
+        avatar: '—ฅ/ᐠ.̫ .ᐟ\\ฅ—',
+        address: 'my Address'
+    },
+    {
+        id: 2,
+        name: 'John',
+        age: 22,
+        avatar: ':)',
+        address: 'California'
+    },
+    {
+        id: 3,
+        name: 'Mike',
+        age: 18,
+        avatar: '^._.^',
+        address: 'New York'
+    },
+    {
+        id: 4,
+        name: 'Emma',
+        age: 38,
+        avatar: '/ᐠ-ꞈ-ᐟ\\',
+        address: 'Washington'
+    },
+]
+
+const StartPage = () => {
+    const navigate = useNavigate()
+    const friends = users.filter(u => u.id !== 1)
+
+    const mappedFriends = friends.map((f, i) => {
+        const go = () => {
+            navigate('/friend/' + f.id)
+        }
+
+        return (
+            <div key={i} onClick={go} style={{paddingLeft: 24, color: 'blue', cursor: 'pointer'}}>
+                {f.name}, {f.age}
+            </div>
+        )
+    })
+
     return (
         <div>
-            <h2>Для чего надо добавлять файлы в .gitignore ?</h2>
-            <ul>
-                <li>1 - Чтобы git удалял их историю, храня только последнюю версию</li>
-                <li>2 - Чтобы git при работе с этими файлам уведомлял при их изменении</li>
-                <li>3 - Чтобы git не следил за изменениями в данных файлах</li>
-                <li>4 - Файл .gitignore не несет никакой смысловой нагрузки, т.к. все файлы с которыми мы работаем должны
-                    отслеживаться. Соответственно никакие файлы в .gitignore добавлять не нужно
-                </li>
-                <li>5 - Нет правильного ответа</li>
-            </ul>
+            <h2>🙂 My profile</h2>
+            <Profile userId={1}/>
+            <hr/>
+            <h2>👪 Friends</h2>
+            {mappedFriends}
+        </div>
+    )
+}
+const Profile: React.FC<{ userId?: number }> = ({userId}) => {
+    const {id} = useParams<{ id: string }>()
+    const user = users.find(u => u.id === +(id || userId || 0))
+
+    return (
+        <div>
+            <div>
+                <b>avatar</b> {user?.avatar}
+            </div>
+            <div>
+                <div><b>name</b>: {user?.name}</div>
+                <div><b>age</b>: {user?.age}</div>
+                <div><b>address</b>: {user?.address}</div>
+            </div>
         </div>
     )
 }
 
+export const Friends = () => {
+    return (
+        <Routes>
+            <Route path={'/'} element={<StartPage/>}/>
+            <Route path={'/friend/:id'} element={<Profile/>}/>
+            <Route path={'*'} element={<div>❌404 Page Not Found❌</div>}/>
+        </Routes>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App/>);
+root.render(
+    <BrowserRouter>
+        <Friends/>
+    </BrowserRouter>
+);
 
 // 📜 Описание:
-// Для чего надо добавлять файлы в .gitignore ?
-// Может быть несколько вариантов ответа (ответ дайте через пробел).
-// ❗ Ответ будет засчитан как верный, только при полном правильном совпадении.
-// Если указали правильно один вариант (1),
-// а нужно было указать два варианта (1 и 2), то ответ в данном случае будет засчитан как неправильный
-
-// 🖥 Пример ответа: 1
+// При загрузке приложения на экране отображается
+// профиль пользователя и список друзей.
+// Если кликнуть на пользователя, то видим ❌404 Page Not Found❌
+// Исправьте код, чтобы по клику на пользователя
+// отображалась странице с информацией о друге.
+// В качестве ответа укажите исправленную строку кода.
+//
+// 🖥 Пример ответа: <Profile userId={4}/>
