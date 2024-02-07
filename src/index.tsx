17f3f3679af7e7afd83c327b11672971e657f336
Pavel Novik <brn2befree@gmail.com>
2024-02-07 23:19:47 +0300

    ex 4

--- src/index.tsx
+++ src/index.tsx
@@ -1,111 +1,60 @@
-import ReactDOM from "react-dom/client";
-import { ThunkAction, ThunkDispatch } from "redux-thunk";
-import { Provider, TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import React, { useState } from "react";
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { useFormik } from 'formik';
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-// Styles
-const modal: React.CSSProperties = {
-    position: "fixed",
-    zIndex: 1,
-    left: 0,
-    top: 0,
-    width: "100%",
-    height: "100%",
-    overflow: "auto",
-    backgroundColor: "rgba(23,26,38,0.26)",
-};
 
-const modalContent: React.CSSProperties = {
-    backgroundColor: "#fefefe",
-    margin: "15% auto",
-    padding: "20px",
-    border: "1px solid #888",
-    width: "80%",
-};
+// Types
+type LoginFieldsType = {
+    firstName?: string
+}
 
-// Reducer
-const initState = { goodThings: [] as any[] };
-type InitStateType = typeof initState;
+// Main
+export const Login = () => {
 
-const appReducer = (state: InitStateType = initState, action: ActionsType): InitStateType => {
-    switch (action.type) {
-        case "LIKE":
-            return {
-                ...state,
-                goodThings: [action.thing, ...state.goodThings],
-            };
-    }
-    return state;
-};
-
-// Store
-const rootReducer = combineReducers({ app: appReducer });
-
-const store = configureStore({ reducer: rootReducer });
-type RootState = ReturnType<typeof store.getState>;
-type AppDispatch = ThunkDispatch<RootState, unknown, ActionsType>;
-type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, ActionsType>;
-const useAppDispatch = () => useDispatch<AppDispatch>();
-const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-
-const addThing = (thing: any) => ({ type: "LIKE", thing }) as const;
-type ActionsType = ReturnType<typeof addThing>;
-
-const Modal = (props: any) => {
-    return (
-        <div style={modalContent}>
-            modal:
-            <input value={props.value} onChange={(e) => props.setValue(e.target.value)} />
-            <button onClick={props.add}>add</button>
-        </div>
-    );
-};
-
-// Components
-export const Animals = () => {
-    const goodThings = useAppSelector((state) => state.app.goodThings);
-    const dispatch = useAppDispatch();
-
-    const [value, setValue] = useState("");
-    const [show, setShow] = useState(false);
-
-    const mapped = goodThings.map((t: any, i: number) => <div key={i}>{t}</div>);
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+        },
+        validate: (values) => {
+            const errors: LoginFieldsType = {};
+            if (values.firstName.length < 5) errors.firstName = 'Must be 5 characters or more'
+            return errors
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        }
+    });
 
     return (
-        <div style={modal}>
-            <button onClick={() => setShow(true)}>show modal</button>
-
-            {show && (
-                <Modal
-                    value={value}
-                    setValue={setValue}
-                    add={() => {
-                        dispatch(addThing(value));
-                        setValue("");
-                        setShow(false)
-                    }}
-                />
-            )}
-
-            {mapped}
-        </div>
+        <form onSubmit={formik.handleSubmit}>
+            <div>
+                <input placeholder={'Введите имя'} {...formik.getFieldProps('firstName')} />
+            </div>
+            <button type="submit" disabled={!(formik.isValid && formik.dirty)}>Отправить</button>
+        </form>
     );
-};
+}
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(
-    <Provider store={store}>
-        <Animals />
-    </Provider>,
-);
+// App
+export const App = () => {
+    return (
+        <Routes>
+            <Route path={''} element={<Login />} />
+        </Routes>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(<BrowserRouter><App /></BrowserRouter>)
 
 // 📜 Описание:
-// Откройте модалку, введите любой текст и нажмите add.
-// Введенный текст отобразится снизу, но модалка останется по прежнему видимой.
+// Начните вводить в поле firstName символы. После ввода первого символа кнопка "Отправить" раздизаблится.
+// Задача: кнопка "Отправить" должна раздизаблиться только в том случае, если длинна имени больше, либо равна 5 символам.
+// Т.е. вам необходимо самостоятельно написать эту валидацию для поля firstName.
+// ❗ В качестве текста ошибки напишите 'Must be 5 characters or more'
+// ❗ Текст ошибки выводить не нужно (только если для себя поиграться).
 
-// 🪛 Задача:
-// Необходимо сделать так, чтобы модалка пряталась сразу после добавления элемента
-// В качестве ответа укажите строку коду, которую необходимо добавить для реализации данной задачи
-
-// 🖥 Пример ответа: closeModal(true)
+// В качестве ответа напишите полностью строку кода с условием.
+// 🖥 Пример ответа: return errors.firstName = 'Must be 5 characters or more'
+// ❗ Сторонние библиотеки (например yup) использовать запрещено
